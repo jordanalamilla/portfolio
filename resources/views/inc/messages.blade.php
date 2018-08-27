53556b9af94e40ec11e87b9883dd39ffbc6287f1
Jordan Alamilla <jordan.alamilla@gmail.com>
2018-08-27 09:53:00 -0400

    updated styles for flashed messages

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,27 +1,41 @@
 @if( session( 'success' ) )
 
-    <div class="alert alert-success">
-        {{ session( 'success' ) }}
+    <div class="light container-small">
+
+        <div class="alert alert-success">
+            {{ session( 'success' ) }}
+        </div>
+
     </div>
 
 @elseif( session( 'error' ) )
 
-    <div class="alert alert-danger">
-        {{ session( 'error' ) }}
+    <div class="light container-small">
+
+        <div class="alert alert-danger">
+            {{ session( 'error' ) }}
+        </div>
+        
     </div>
 
 @endif
 
 @if ( $errors->any() )
 
-    <div class="alert alert-danger">
+    <div class="light container-small">
 
-        <ul>
+        <div class="alert alert-danger">
 
-            @foreach ( $errors->all() as $error )
-                <li>{{ $error }}</li>
-            @endforeach
+            <ul>
 
-        </ul>
+                @foreach ( $errors->all() as $error )
+                    <li>{{ $error }}</li>
+                @endforeach
+
+            </ul>
+
+        </div>
+
     </div>
+
 @endif
